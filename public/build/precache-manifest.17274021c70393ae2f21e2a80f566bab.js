2dd945168dca13ff5766f3dd93042a861838fdb7
NaveenaTsMaximizer <naveena@maximizer-e-services.com>
2019-03-03 04:09:01 +0530

    first commit

---
+++ public/build/precache-manifest.17274021c70393ae2f21e2a80f566bab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "79e527655d6d4b25b079",
+    "url": "/static/js/main.79e52765.chunk.js"
+  },
+  {
+    "revision": "35b720f79b0f8bb89d48",
+    "url": "/static/js/2.35b720f7.chunk.js"
+  },
+  {
+    "revision": "79e527655d6d4b25b079",
+    "url": "/static/css/main.cb844c13.chunk.css"
+  },
+  {
+    "revision": "54e0c78929929064a9e8419031668332",
+    "url": "/index.html"
+  }
+];
